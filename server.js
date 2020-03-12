99733e56c61f33e042425ae04dee6926950f899c
Neverender <tldavis09@yahoo.com>
2020-03-11 19:56:16 -0500

    slowly adding. Need to work out the post

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require("fs").promises;
 const path = require("path");
 const express = require("express");
 const app = express();
@@ -14,5 +15,11 @@ app.get("/", (req, res) => {
 app.get("/notes", (req, res) => {
 	res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
+
+app.get("/api/notes", (req, res) => {
+	res.sendfile(path.join(__dirname, "/db/db.json"));
+});
+
+app.post("/api/notes", (req, res) => {});
 
 app.listen(PORT, () => console.log(`Listening on port:${PORT}....`));
